856c594445a81db98755424949810cfc576e3f9f
NicolasLerch <lerchnicolas@hotmail.com>
2024-02-08 10:29:48 -0300

    Establecida config para iniciar servidor

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.json());
+
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+app.listen(3000, () =>{
+    console.log('Servidor corriendo en el puerto 3000')
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+});
