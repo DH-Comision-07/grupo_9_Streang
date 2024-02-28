5a50b4ff611429c11b10a3ff7b66286ed097afa0
Tatizao1 <laboratorioalvadent627@gmail.com>
2024-02-28 19:23:02 -0300

    modifique parametros de app.js y añadi register en css y views

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
 app.listen(3000, () =>{
-    console.log('Servidor corriendo en el puerto 3000')
+    console.log(`Servidor corriendo en Puerto ${port}`)
 });
 
+// route para pagina raiz
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
 });
@@ -18,4 +19,14 @@ app.get('/', (req, res) => {
 // route para pagina de producto
 app.get('/producto', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/productDetail.html'))
+});
+
+// router para pagina de registro
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+});
+
+// router para pagina de carrito
+app.get('/cart', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/cart.html'))
 });
