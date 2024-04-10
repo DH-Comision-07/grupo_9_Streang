74dd1eae7f245876549018b1c75c7232045538c4
NicolasLerch <lerchnicolas@hotmail.com>
2024-04-10 17:53:26 -0300

    creada la vista de perfil de usuario

--- routes/mainRoutes.routes.js
+++ routes/mainRoutes.routes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const indexRouter = require('./index.routes');
 const productsRouter = require('./products.routes');
+const usersRouter = require('./users.routes');
 
 router.use('/', indexRouter);
-router.use('/products', productsRouter)
+router.use('/products', productsRouter);
+router.use('/users', usersRouter);
 
 module.exports = router;
