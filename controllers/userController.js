2b346d9d76aa150100292e903c8f03b383aad70e
Tatizao1 <laboratorioalvadent627@gmail.com>
2024-04-10 21:31:49 -0300

    register usuarios develop

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,14 @@
 const userController = {
     viewProfile : function (req, res) {
         res.render("userProfile");
-    }
+    },
+    saveUser: function (req,res) {
+        console.log(req.body)
+        res.send(req.body)
+    },
+    registerView: function (req,res) {
+        res.render('register');
+    },
 };
 
 module.exports = userController;
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const mainController = require('../controllers/mainController');
 
 router.get('/profile', userController.viewProfile);
+
+router.get('/register', userController.registerView)
+router.post('/register', userController.saveUser)
 
 module.exports = router;
