20f0abf9f25acab9a0c94e38208d1f43dc5b0063
Tatizao1 <laboratorioalvadent627@gmail.com>
2024-04-10 23:20:26 -0300

    listo post de usuarios json falta multer

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const usersFilePath = path.join(__dirname, '../data/users.json');
+const usersDataBase = require('../data/users.json')
+users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'))
+
 const userController = {
     viewProfile : function (req, res) {
         res.render("userProfile");
     },
-    saveUser: function (req,res) {
-        console.log(req.body)
-        res.send(req.body)
+    saveUser: (req,res) => {
+        let maxId = 0;
+        for (const obj of users) {
+            if (obj.id && obj.id > maxId) {
+                maxId = obj.id;
+            }
+        }
+        let newUserInfo = {
+            id: maxId + 1,
+            email: req.body.email,
+            username: req.body.username,
+            password: req.body.password,
+            name: req.body.realName,
+            surname: req.body.surname,
+        }
+        users.push(newUserInfo)
+        let usersJSON = JSON.stringify(users)
+        fs.writeFileSync(usersFilePath, usersJSON)
+        res.send(usersJSON)
     },
     registerView: function (req,res) {
         res.render('register');
