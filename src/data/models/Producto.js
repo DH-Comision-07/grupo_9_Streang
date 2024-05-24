76671e9da353d2f8233cbaa2c977c7dd01a0b8a6
Damucho <damis96@hotmail.com.ar>
2024-05-23 23:07:41 -0300

    iniciada configuración de producto y compra

---
+++ src/data/models/Producto.js
@@ -0,0 +1,57 @@
+module.exports = function(sequelize, dataTypes){
+    let alias = "Producto";
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        marca: {
+            type: dataTypes.STRING(45),
+            allowNull: false
+        },
+
+        nombre: {
+            type: dataTypes.STRING(60),
+            allowNull: false
+        },
+
+        precio: {
+            type: dataTypes.INTEGER,
+            allowNull: false
+        },
+
+        disponible: {
+            type: dataTypes.BOOLEAN,
+            allowNull: false
+        },
+
+        imagenes: {
+            type: dataTypes.BOOLEAN,
+            allowNull: true
+        },
+
+        categoria: {
+            type: dataTypes.ENUM('videojuegos', 'consolas', 'accesorios'),
+            allowNull: false
+        }
+    }
+    let config = {
+        tableName: 'productos',
+        timestamps: false
+    }
+
+    let Producto = sequelize.define(alias, cols, config);
+
+    Producto.associate = function(models){
+        Producto.belongsToMany(models.Compra, {
+            as: "compras",
+            through: "productos_compras",
+            foreignKey: "productos_id",
+            otherKey: "compras_id",
+            timestamps: false
+        })
+    }
+}
