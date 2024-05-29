c42d8ab9c37ffb4d7f2656a279bd425d334501d6
Tatizao1 <laboratorioalvadent627@gmail.com>
2024-05-29 19:55:33 -0300

    creadas las tablas buy y categorie

---
+++ src/data/models/Buy.js
@@ -0,0 +1,31 @@
+module.exports = function(sequelize, dataTypes) {
+    let alias = "Buys"
+    let cols = {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false,
+        },
+        date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        buyscol: {
+            type: DataTypes.STRING(45),
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        product_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        total: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    };
+}
---
+++ src/data/models/Categorie.js
@@ -0,0 +1,15 @@
+module.exports = function(sequelize, dataTypes) {
+    let alias = "Categories";
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            allownull: false,
+            autoIncrement: true, 
+            primaryKey: true, 
+        },
+        name: {
+            type: dataTypes.STRING(45),
+            allownull: false,
+        }
+    }
+}
