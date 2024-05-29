6d6b0205908888efa934830803f2e3b48dae6f29
Damucho <damis96@hotmail.com.ar>
2024-05-29 20:51:38 -0300

    fix: date

--- src/data/models/Buy.js
+++ src/data/models/Buy.js
@@ -9,7 +9,7 @@ module.exports = function(sequelize, dataTypes){
         },
 
         date: {
-            type: dataTypes.DATETIME,
+            type: dataTypes.DATE,
             allowNull: false
         },
 
