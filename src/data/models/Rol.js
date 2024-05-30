e40fe5cec7bb36f0fe4d88c3184f32daf4f5d8f0
Damucho <damis96@hotmail.com.ar>
2024-05-30 19:29:29 -0300

    creado modelo Rol

---
+++ src/data/models/Rol.js
@@ -0,0 +1,32 @@
+module.exports = function(sequelize, dataTypes){
+    let alias = "rols";
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+
+        name: {
+            type: dataTypes.STRING,
+            allowNull: false
+        }
+    }
+
+    let config = {
+        tableName: 'rols',
+        timestamps: false
+    }
+
+    let Rol = sequelize.define(alias, cols, config);
+    
+    // Rol.associate = function(models){
+    //     Rol.hasMany(models.User, {
+    //         foreignKey: "rol_id",
+    //         as: "rols"
+    //     })
+    // }
+
+    return Rol
+}
