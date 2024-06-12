a275949ba0641170863864b7a13b8b1700aa5ab0
Tatizao1 <laboratorioalvadent627@gmail.com>
2024-06-12 17:06:50 -0300

    validations products middleware

--- src/middlewares/productValidation.js
+++ src/middlewares/productValidation.js
@@ -1,7 +1,29 @@
 const { check } = require('express-validator');
 
 const validateProduct = [
+    check('mainImage').notEmpty()
+    .withMessage('Introduzca una Imagen'),
 
+    check('video').notEmpty()
+    .withMessage('Introduzca un link'),
+
+    check('name').notEmpty()
+    .withMessage('Inserte el nombre del producto'),
+
+    check('name').isLength({min: 3})
+    .withMessage('El nombre del producto tiene que tener un minimo de 3 caracteres'),
+
+    check('description').notEmpty()
+    .withMessage('Descripcion requerida'),
+
+    check('descripcion').isLength({min: 13})
+    .withMessage('La descripcion del producto tiene que tener un minimo de 13 caracteres'),
+    
+    check('price').notEmpty()
+    .withMessage('Inserte precio para el producto'),
+
+    check('format').notEmpty()
+    .withMessage("Inserte un formato para el producto"),
 ];
 
 module.exports = validateProduct;
